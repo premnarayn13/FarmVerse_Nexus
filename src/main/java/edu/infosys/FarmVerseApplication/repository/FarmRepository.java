767b26a4ebcd21a214807d0db4a773f6dcca4f98
prem <naraynpremnl1304@gmail.com>
2026-07-14 21:35:13 +0530

    Wrote the code in FarmRepository

--- src/main/java/edu/infosys/FarmVerseApplication/repository/FarmRepository.java
+++ src/main/java/edu/infosys/FarmVerseApplication/repository/FarmRepository.java
@@ -1,4 +1,20 @@
 package edu.infosys.FarmVerseApplication.repository;
 
-public interface FarmRepository {
+/*public interface FarmRepository {
+}
+ */
+//package edu.infosys.farmVerseApplication.dao;
+
+import edu.infosys.FarmVerseApplication.entity.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface FarmRepository extends JpaRepository<Farm, Long> {
+
+    @Query("Select max(farmId) from Farm")
+    public Long getMaxFarmId();
+    @Query("Select username from Farm where username=?1")
+    public List<Farm> getFarmsByUsername(String username);
 }
