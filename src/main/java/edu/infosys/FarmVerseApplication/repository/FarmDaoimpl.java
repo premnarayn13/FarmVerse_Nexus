6f40041387119386c08a8be6ff0060faf1e1c1d1
prem <naraynpremnl1304@gmail.com>
2026-07-15 20:48:25 +0530

    Added code on FarmDaoimpl

--- src/main/java/edu/infosys/FarmVerseApplication/repository/FarmDaoimpl.java
+++ src/main/java/edu/infosys/FarmVerseApplication/repository/FarmDaoimpl.java
@@ -4,29 +4,46 @@ import edu.infosys.FarmVerseApplication.entity.Farm;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
+@Service
 public class FarmDaoimpl implements FarmDao {
+
+    @Autowired
+    private FarmRepository repository;
+
     @Override
     public void addFarm(Farm farm) {
+        // TODO Auto-generated method stub
+        repository.save(farm);
 
     }
 
     @Override
     public Farm getFarmById(Long id) {
-        return null;
+        // TODO Auto-generated method stub
+        return repository.findById(id).get();
     }
 
     @Override
     public List<Farm> getFarmsByUsername(String username) {
-        return List.of();
+        // TODO Auto-generated method stub
+        return repository.getFarmsByUsername(username);
     }
 
     @Override
     public void deleteFarmById(Long id) {
-
+        // TODO Auto-generated method stub
+        repository.deleteById(id);
     }
 
     @Override
     public Long getMaxFarmId() {
-        return 0L;
+        // TODO Auto-generated method stub
+        return repository.getMaxFarmId();
     }
+
 }
