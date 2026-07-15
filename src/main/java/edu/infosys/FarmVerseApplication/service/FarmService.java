71bf96ab5dd08689f7fd109eb6c06c7b963550de
prem <naraynpremnl1304@gmail.com>
2026-07-15 21:06:07 +0530

    Updated the Farmservice code

--- src/main/java/edu/infosys/FarmVerseApplication/service/FarmService.java
+++ src/main/java/edu/infosys/FarmVerseApplication/service/FarmService.java
@@ -1,6 +1,24 @@
 package edu.infosys.FarmVerseApplication.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.infosys.FarmVerseApplication.repository.FarmDao;
+
+@Service
 public class FarmService {
+
+    @Autowired
+    private FarmDao farmDao;
+
     public Long generateFarmId() {
+        Long value=farmDao.getMaxFarmId();
+        if(value==null)
+            value=10001L;
+        else
+            value=value+1;
+
+        return value;
     }
+
 }
