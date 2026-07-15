1135ae01d029cc29a6cba2a24bd68adcfd011c82
prem <naraynpremnl1304@gmail.com>
2026-07-15 20:50:34 +0530

    Updated the code of FarmDaoimpl

--- src/main/java/edu/infosys/FarmVerseApplication/repository/FarmDaoimpl.java
+++ src/main/java/edu/infosys/FarmVerseApplication/repository/FarmDaoimpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+
 @Repository
 @Service
 public class FarmDaoimpl implements FarmDao {
