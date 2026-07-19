3fb1ff21a3aa891fc171ff1232b8b16da7220601
prem <naraynpremnl1304@gmail.com>
2026-07-20 00:14:27 +0530

    Updated the code of CropRepository.java

--- src/main/java/edu/infosys/FarmVerseApplication/repository/CropRepository.java
+++ src/main/java/edu/infosys/FarmVerseApplication/repository/CropRepository.java
@@ -1,4 +1,22 @@
 package edu.infosys.FarmVerseApplication.repository;
 
-public interface CropRepository {
+//package edu.infosys.farmVerseApplication.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import edu.infosys.FarmVerseApplication.entity.Crop;
+
+public interface CropRepository extends JpaRepository<Crop, String> {
+
+
+    @Query(value = "SELECT MAX(CAST(SUBSTRING(cropId, 2) AS UNSIGNED)) FROM crop", nativeQuery = true)
+    public Long getMaxCropId();
+
+    @Query("Select a from Crop a where a.username=?1")
+    public List<Crop> getCropsByUsername(String username);
+
 }
+
