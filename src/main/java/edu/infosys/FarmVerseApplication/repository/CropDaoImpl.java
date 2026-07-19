48d0d9730b03596eb92236bb4e3792414eff6463
prem <naraynpremnl1304@gmail.com>
2026-07-20 00:12:52 +0530

    Updated the code in CropDaoImpl

--- src/main/java/edu/infosys/FarmVerseApplication/repository/CropDaoImpl.java
+++ src/main/java/edu/infosys/FarmVerseApplication/repository/CropDaoImpl.java
@@ -1,4 +1,52 @@
 package edu.infosys.FarmVerseApplication.repository;
 
-public class CropDaoImpl {
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import edu.infosys.FarmVerseApplication.entity.Crop;
+
+@Repository
+@Service
+public class CropDaoImpl implements CropDao {
+
+    @Autowired
+    private CropRepository repository;
+
+    @Override
+    public void addCrop(Crop crop) {
+        // TODO Auto-generated method stub
+        repository.save(crop);
+    }
+
+    @Override
+    public Crop getCropById(String id) {
+        // TODO Auto-generated method stub
+        return repository.findById(id).get();
+    }
+
+    @Override
+    public List<Crop> getCropsByUsername(String username) {
+        // TODO Auto-generated method stub
+        return repository.getCropsByUsername(username);
+    }
+
+    @Override
+    public void deleteCropById(String id) {
+        // TODO Auto-generated method stub
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Long getMaxCropId() {
+        // TODO Auto-generated method stub
+        return repository.getMaxCropId();
+    }
+
+
+
+
 }
