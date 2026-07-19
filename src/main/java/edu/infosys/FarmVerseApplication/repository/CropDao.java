54788b4de52d15e62f2cd45d6fa81eae265022e0
prem <naraynpremnl1304@gmail.com>
2026-07-20 00:11:20 +0530

    Updated the code in CropDao in repository folder

--- src/main/java/edu/infosys/FarmVerseApplication/repository/CropDao.java
+++ src/main/java/edu/infosys/FarmVerseApplication/repository/CropDao.java
@@ -1,4 +1,14 @@
 package edu.infosys.FarmVerseApplication.repository;
 
+import java.util.List;
+
+import edu.infosys.FarmVerseApplication.entity.Crop;
+
 public interface CropDao {
+
+    public void addCrop(Crop crop);
+    public Crop getCropById(String id);
+    public List<Crop> getCropsByUsername(String username);
+    public void deleteCropById(String id);
+    public Long getMaxCropId();
 }
