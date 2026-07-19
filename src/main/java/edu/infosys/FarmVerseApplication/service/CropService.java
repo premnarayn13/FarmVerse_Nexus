096c486aaf8585e35d32a437560442597e2471b7
prem <naraynpremnl1304@gmail.com>
2026-07-20 00:08:45 +0530

    Updated code in CropService.java in service folder

--- src/main/java/edu/infosys/FarmVerseApplication/service/CropService.java
+++ src/main/java/edu/infosys/FarmVerseApplication/service/CropService.java
@@ -1,4 +1,39 @@
 package edu.infosys.FarmVerseApplication.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.infosys.FarmVerseApplication.entity.Crop;
+import edu.infosys.FarmVerseApplication.repository.CropDao;
+
+@Service
 public class CropService {
+
+    @Autowired
+    private CropDao cropDao;
+
+    @Autowired
+    private FarmUserService service;
+
+    public String generateCropId() {
+        Long value = cropDao.getMaxCropId();
+
+        if (value == null)
+            value = 1000001L;
+        else
+            value = value + 1;
+
+        String newId = "C"+value;
+
+        return newId;
+    }
+
+    public Crop setUsername(Crop crop) {
+        String username = service.getUserId();
+        crop.setUsername(username);
+        return crop;
+    }
+
 }
+
