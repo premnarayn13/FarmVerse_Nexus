3ac5af2d2832e86e4efbe63093a665ae62db6898
prem <naraynpremnl1304@gmail.com>
2026-08-05 00:09:10 +0530

    Created new AgroExepnseService file

--- src/main/java/edu/infosys/FarmVerseApplication/controller/AgroExpenseController.java
+++ src/main/java/edu/infosys/FarmVerseApplication/controller/AgroExpenseController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import edu.infosys.FarmVerseApplication.entity.AgroExpense;
-import edu.infosys.FarmVerseApplication.entity.AgroExpenseDao;
+import edu.infosys.FarmVerseApplication.repository.AgroExpenseDao;
 import edu.infosys.FarmVerseApplication.service.AgroExpenseService;
 
 @RestController
