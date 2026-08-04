078b59bab01fb852d4757ba013bc604d55718617
prem <naraynpremnl1304@gmail.com>
2026-08-04 23:55:01 +0530

    Added new code in AgroExpenseController

--- src/main/java/edu/infosys/FarmVerseApplication/controller/AgroExpenseController.java
+++ src/main/java/edu/infosys/FarmVerseApplication/controller/AgroExpenseController.java
@@ -1,4 +1,53 @@
 package edu.infosys.FarmVerseApplication.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import edu.infosys.FarmVerseApplication.entity.AgroExpense;
+import edu.infosys.FarmVerseApplication.entity.AgroExpenseDao;
+import edu.infosys.FarmVerseApplication.service.AgroExpenseService;
+
+@RestController
+@RequestMapping("/farmverse/")
+@CrossOrigin(origins = "http://localhost:3636", allowCredentials = "true")
 public class AgroExpenseController {
+
+    @Autowired
+    private AgroExpenseDao agroExpenseDao;
+
+    @Autowired
+    private AgroExpenseService service;
+
+    @PostMapping("/agroexpense")
+    public void addAgroExpense(@RequestBody AgroExpense agroExpense) {
+        agroExpenseDao.addAgroExpense(agroExpense);
+    }
+
+    @PutMapping("/agroexpense")
+    public void updateAgroExpense(@RequestBody AgroExpense agroExpense) {
+        agroExpenseDao.addAgroExpense(agroExpense);
+    }
+
+    @GetMapping("/agroexpense/{id}")
+    public AgroExpense getAgroExpenseById(@PathVariable Integer id) {
+        return agroExpenseDao.getAgroExpenseById(id);
+    }
+
+    @GetMapping("/agroexpense")
+    public List<AgroExpense> getAllAgroExpenses() {
+        return agroExpenseDao.getAllAgroExpenses();
+    }
+
+    @DeleteMapping("/agroexpense/{id}")
+    public void deleteAgroExpenseById(@PathVariable Integer id) {
+        agroExpenseDao.deleteAgroExpenseById(id);
+    }
+
+    @GetMapping("/agroexpense-id")
+    public Integer generateExpenseId() {
+        return service.generateExpenseId();
+    }
+
 }
