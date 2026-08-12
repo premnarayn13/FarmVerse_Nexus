7ee06fcd490d83054a2020377dafaebe847afd3a
prem <naraynpremnl1304@gmail.com>
2026-08-12 20:46:18 +0530

    Added code in CropInputsDaoImpl

--- src/main/java/edu/infosys/FarmVerseApplication/repository/CropInputsDaoImpl.java
+++ src/main/java/edu/infosys/FarmVerseApplication/repository/CropInputsDaoImpl.java
@@ -1,4 +1,32 @@
 package edu.infosys.FarmVerseApplication.repository;
 
-public class CropInputsDaoImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import edu.infosys.FarmVerseApplication.entity.CropInputs;
+import edu.infosys.FarmVerseApplication.entity.Farm;
+
+@Repository
+@Service
+public class CropInputsDaoImpl implements CropInputsDao {
+
+    @Autowired
+    private CropInputsRepository repository;
+
+    @Override
+    public void addCropInputs(CropInputs cropInputs) {
+        repository.save(cropInputs);
+    }
+
+    @Override
+    public CropInputs getCropInputsById(String cropId) {
+        return repository.findById(cropId).get();
+    }
+
+    @Override
+    public void deleteCropInputsById(String cropId) {
+        repository.deleteById(cropId);
+    }
+
 }
